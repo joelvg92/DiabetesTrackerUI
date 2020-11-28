2bbb0100ed204769d01407a38fd8c8f4c7ddc455
Joel Varghese <jvarghese@ms3-inc.com>
2020-11-28 09:17:19 -0500

    Initial commit

---
+++ js/sb-admin-2.js
@@ -0,0 +1,77 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Toggle the side navigation
+  $("#sidebarToggle, #sidebarToggleTop").on('click', function(e) {
+    $("body").toggleClass("sidebar-toggled");
+    $(".sidebar").toggleClass("toggled");
+    if ($(".sidebar").hasClass("toggled")) {
+      $('.sidebar .collapse').collapse('hide');
+    };
+  });
+
+  // Close any open menu accordions when window is resized below 768px
+  $(window).resize(function() {
+    if ($(window).width() < 768) {
+      $('.sidebar .collapse').collapse('hide');
+    };
+    
+    // Toggle the side navigation when window is resized below 480px
+    if ($(window).width() < 480 && !$(".sidebar").hasClass("toggled")) {
+      $("body").addClass("sidebar-toggled");
+      $(".sidebar").addClass("toggled");
+      $('.sidebar .collapse').collapse('hide');
+    };
+  });
+
+  // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
+  $('body.fixed-nav .sidebar').on('mousewheel DOMMouseScroll wheel', function(e) {
+    if ($(window).width() > 768) {
+      var e0 = e.originalEvent,
+        delta = e0.wheelDelta || -e0.detail;
+      this.scrollTop += (delta < 0 ? 1 : -1) * 30;
+      e.preventDefault();
+    }
+  });
+
+  // Scroll to top button appear
+  $(document).on('scroll', function() {
+    var scrollDistance = $(this).scrollTop();
+    if (scrollDistance > 100) {
+      $('.scroll-to-top').fadeIn();
+    } else {
+      $('.scroll-to-top').fadeOut();
+    }
+  });
+
+  // Smooth scrolling using jQuery easing
+  $(document).on('click', 'a.scroll-to-top', function(e) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: ($($anchor.attr('href')).offset().top)
+    }, 1000, 'easeInOutExpo');
+    e.preventDefault();
+  });
+
+  $("form#loginForm").submit(function() {
+    var username = $('#username').val();
+    var password = $('#password').val();
+    $.ajax({
+      type: "POST",
+      url: "http://localhost:8080/login",
+      data: JSON.stringify({ username: username, password: password }),
+      contentType: 'application/json',
+      success: function(response, textStatus, xhr) {
+        console.log(response);
+        localStorage.setItem("user", response.patientId);
+        window.location = "index.html?user="+response.patientId;
+      },
+      error: function(xhr, textStatus, errorThrown) {
+        console.log("error");
+        $("#errorMessage").removeClass('d-none');
+      }
+    });
+    return false;
+  });
+
+})(jQuery); // End of use strict
